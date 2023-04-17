0954c65e6794130ad83ff17c0d631c0c5687541d
Nick Valdez <nicholastvaldez@gmail.com>
2023-04-17 12:51:26 -0500

    window dimensions and orientation hooks

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  Alert,
+  Dimensions,
+  SafeAreaView,
+  Button,
+  useWindowDimensions,
+} from "react-native"
+import { useDeviceOrientation } from "@react-native-community/hooks"
 
 export default function App() {
+  const orientation = useDeviceOrientation()
+  console.log(useWindowDimensions())
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: orientation === "landscape" ? "100%" : "30%",
+        }}
+      ></View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
-});
+})

8b5479b3f16aa61b2f9b3b1390c343618314f0fe
Nick Valdez <nicholastvaldez@gmail.com>
2023-04-17 16:28:13 -0500

    shadows

--- App.js
+++ App.js
@@ -3,5 +3,25 @@ import WelcomeScreen from "./app/screens/WelcomeScreen"
 import ViewImageScreen from "./app/screens/ViewImageScreen"
 
 export default function App() {
-  return <ViewImageScreen />
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          shadowColor: "grey",
+          shadowOffset: { width: 0, height: 10 },
+          shadowOpacity: 1,
+          shadowRadius: 10,
+        }}
+      ></View>
+    </View>
+  )
 }

c61a1cffe07f3258ab51e1a2f329b9571d2ce5bf
Nick Valdez <nicholastvaldez@gmail.com>
2023-04-19 14:15:09 -0500

    implement listing details component

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import { StyleSheet, Text, View } from "react-native"
 
 import WelcomeScreen from "./app/screens/WelcomeScreen"
 import ViewImageScreen from "./app/screens/ViewImageScreen"
-import AppText from "./app/components/AppText"
-import AppButton from "./app/components/AppButton"
+import Card from "./app/components/Card"
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen"
 
 export default function App() {
-  return <WelcomeScreen />
+  return <ViewImageScreen />
 }
